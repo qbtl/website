e7eb341356f16ba470ee0c9ff9cd5d1f4aea6cdc
qbtl <54680442+qbtl@users.noreply.github.com>
2020-08-19 10:59:41 +0200

    feat: website rebuild process, coming soon page online

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,4 +1,4 @@
-var deadline = new Date("Jul 11, 2020 16:00:00").getTime();
+var deadline = new Date("Aug 31, 2020 21:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var t = deadline - now;
