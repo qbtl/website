899c90ae6b048d2637df47baeadaa05465d87232
qbtl <qbtl.github@gmail.com>
2020-09-17 22:52:03 +0200

    chore: bump file version

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 /*
-Package Name: Website v1.2.3
+Package Name: Website v1.3.0
 Package URI: https://quentinboitel.netlify.app/
 Description: The Package for my website
 Author: Quentin Boitel
-Version: 1.2.3
+Version: 1.3.0
 */
 
 /* Dark Theme Toggle */
