1429ac349d3d68b1ca434ead08a81cc047d4c969
qbtl <qbtl.github@gmail.com>
2021-06-01 03:29:52 +0200

    Portfolio new version

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,37 +3,108 @@
     Package URI: https://quentinboitel.live/assets/js/app.js
     Description: Webiste Package
     Author: Quentin Boitel
-    Version: 3.0.0
 */
-/** When the page is loaded */
-
 
 /**
- * @param {innerHTML} forms Fetch all forms to validate
+ * Calcul la position de l'élément par rapport au haut de la page
+ * @param {HTMLElement} element
+ * @return {number}
  */
-(function () {
-    'use strict'
+function offsetTop(element, acc = 0) {
+    if (element.offsetParent) {
+        return offsetTop(element.offsetParent, acc + element.offsetTop);
+    }
+    return acc + element.offsetTop;
+}
 
-    var forms = document.querySelectorAll('.needs-validation')
-    // Loop over forms and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
+/**
+ * @property {HTMLElement} element
+ * @property {{y: number, r: number, variable: boolean}} options
+ */
+class Parallax {
+    /**
+     * @param {HTMLElement} element
+     */
+    constructor(element) {
+        this.element = element;
+        this.options = this.parseAttribute();
+
+        this.onScroll = this.onScroll.bind(this);
+        this.onIntersection = this.onIntersection.bind(this);
+        this.onResize = this.onResize.bind(this);
+
+        this.elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+        const observer = new IntersectionObserver(this.onIntersection);
+        observer.observe(element);
+        this.onScroll();
+    }
+
+    parseAttribute() {
+        const defaultOptions = {
+            y: 0.2,
+            r: 0,
+            variable: false,
+        };
+        if (this.element.dataset.parallax.startsWith("{")) {
+            return {
+                ...defaultOptions,
+                ...JSON.parse(this.element.dataset.parallax),
+            };
+        }
+        return {
+            ...defaultOptions,
+            y: parseFloat(this.element.dataset.parallax)
+        };
+    }
+
+    /**
+     * @param {IntersectionObserverEntry[]} entries
+     */
+    onIntersection(entries) {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                document.addEventListener("scroll", this.onScroll);
+                window.addEventListener("resize", this.onResize);
+                this.elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+            } else {
+                document.removeEventListener("scroll", this.onScroll);
+                window.removeEventListener("resize", this.onResize);
+            }
+        }
+    }
+
+    onResize() {
+        this.elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+        this.onScroll();
+    }
+
+    onScroll() {
+        window.requestAnimationFrame(() => {
+            const screenY = window.scrollY + window.innerHeight / 2;
+            const diffY = this.elementY - screenY;
+            const translateY = diffY * -1 * this.options.y;
+            if (this.options.variable) {
+                this.element.style.setProperty("--parallaxY", `${translateY}px`);
+            } else if (diffY > 0) {
+                let transform = `translateY(${translateY}px)`;
+                if (this.options.r) {
+                    transform += ` rotate(${diffY * this.options.r}deg)`;
                 }
-                form.classList.add('was-validated')
-            }, false)
-        })
-})();
+                this.element.style.setProperty("transform", transform);
+            }
+        });
+    }
 
-/**
- * @param {innerHTML} button Back to top button
- */
-/*back_top = document.getElementById("back-top");
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}*/
+    /**
+     * @returns {Parallax[]}
+     */
+    static bind() {
+        return Array.from(document.querySelectorAll("[data-parallax]")).map(
+            (element) => {
+                return new Parallax(element);
+            }
+        );
+    }
+}
+
+Parallax.bind();
